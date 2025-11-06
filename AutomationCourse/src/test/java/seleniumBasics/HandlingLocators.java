8ddd482552e5f7308a619d841259e8e9dacf9140
Reshma <goreshma42@gmail.com>
2025-11-06 20:16:59 +0530

    First Commit

---
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingLocators.java
@@ -0,0 +1,25 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+
+public class HandlingLocators extends Base {
+         public void verifyLocator() {
+        	driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php"); 
+        	driver.findElement(By.id("button-one"));
+        	driver.findElement(By.tagName("button"));
+        	driver.findElement(By.className("btn btn-primary"));
+        	driver.findElement(By.name("viewport"));
+        	driver.findElement(By.linkText("Ajax Form Submit"));
+        	driver.findElement(By.partialLinkText("Ajax"));
+        	driver.findElement(By.cssSelector("button[id='button-one']"));
+        	driver.findElement(By.xpath("//input[@id='single-input-field']"));
+        	driver.findElement(By.xpath("//button[text()='Show Message']"));
+        	driver.findElement(By.xpath("//button[@id='button-one' and @type='button']"));
+        	driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));
+         }
+	public static void main(String[] args) {
+		  
+
+	}
+
+}
